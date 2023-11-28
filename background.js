168ec01346eee5871a3f626d664f647467a9d845
tiwei0 <tiwei@posteo.de>
2023-11-28 22:40:11 +0100

    Add stuff

--- background.js
+++ background.js
@@ -1,19 +1,36 @@
-/*
-Create the open link context menu item.
-*/
-browser.contextMenus.create({
-    id: "open-link",
-    title: browser.i18n.getMessage("openCustomLink"),
-    contexts: ["link"]
-}, () => void browser.runtime.lastError,
-);
+
+async function addMenuEntries() {
+    /*
+    Create the open link context menu item.
+    */
+    const storageContent = (await (browser.storage.local.get("concatLink"))).concatLink;
+    if (storageContent != null) {
+        browser.contextMenus.create({
+            id: "open-link",
+            title: `${browser.i18n.getMessage("openConcatLink")} ${storageContent.name ?? storageContent.url}`,
+            contexts: ["link"]
+        },
+        );
+        browser.contextMenus.update(
+            "open-link",
+            {
+                title: `${browser.i18n.getMessage("openConcatLink")} ${storageContent.name ?? storageContent.url}`,
+            }
+        );
+
+    }
+
+
+}
+addMenuEntries();
+
 
 /*
-Create the create context menu item.
+Create the edit context menu item.
 */
 browser.contextMenus.create({
     id: "create-link",
-    title: browser.i18n.getMessage("createCustomLink"),
+    title: browser.i18n.getMessage("editConcatLink"),
     contexts: ["link"]
 }, () => void browser.runtime.lastError,
 );
@@ -22,27 +39,29 @@ let inputForm = {
     type: "detached_panel",
     url: "form.html",
     width: 250,
-    height: 100,
+    height: 200,
     focused: true,
 };
 
 /*
-The message listener.
+The message listener adds link to local storage.
 */
-browser.runtime.onMessage.addListener((request) => browser.storage.local.set({ url: request.url }));
+browser.runtime.onMessage.addListener((request) => { browser.storage.local.set({ concatLink: { name: request.name, url: request.url } }); addMenuEntries() });
 
 /*
 The click event listener.
 */
-browser.contextMenus.onClicked.addListener(async (info, tab) => {
+browser.contextMenus.onClicked.addListener(async (info) => {
     switch (info.menuItemId) {
+        case "open-link":
+            const url = (await browser.storage.local.get("concatLink"))?.concatLink?.url;
+            if (url == null) return;
+            browser.tabs.create({
+                "url": url + info.linkUrl
+            });
+            break;
         case "create-link":
             browser.windows.create(inputForm);
-            break;
-            case "open-link":
-            browser.tabs.create({
-                "url": (await browser.storage.local.get("url")).url + info.linkUrl
-            });
             break;
     }
 });
--- form.js
+++ form.js
@@ -1,5 +1,27 @@
-document.getElementById("form").addEventListener("keyup", () => {
-  const url = document.getElementById('form').value;
-  document.getElementById("result").innerHTML = url;
-  browser.runtime.sendMessage({url});
-});
+const nameInput = document.getElementById("name");
+const urlInput = document.getElementById("url");
+const okBtn = document.getElementById("ok");
+
+nameInput.setAttribute('placeholder',browser.i18n.getMessage("name"))
+urlInput.setAttribute('placeholder',browser.i18n.getMessage("url"))
+
+function change() {
+  const url = urlInput.value;
+  const name = nameInput.value;
+  if(url?.length > 2){
+  browser.runtime.sendMessage({name, url});
+  document.getElementById("result").innerHTML = `${browser.i18n.getMessage("saved")} ${name} -> ${url}`;
+  setTimeout(async ()=> browser.windows.remove(
+    (await browser.windows.getCurrent()).id
+  ),2000);
+}
+}
+async function setData(){
+  const storageContent = (await (browser.storage.local.get("concatLink")))?.concatLink;
+  urlInput.value = storageContent?.url ?? '';
+  nameInput.value = storageContent?.name ?? '';
+}
+
+setData();
+okBtn.addEventListener("click",change);
+nameInput.focus();
